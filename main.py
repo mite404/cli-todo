cf1f0de7942a6f057680ac1343f1a8c7d13c8bdb
mite404 <ethana2008@gmail.com>
2024-06-08 13:48:59 -0400

    Added .json read/write functionality.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #
 #    CLI TO DO App
 #
+import json
 
 class Task:
     def __init__(self, title: str, description: str, is_complete: bool):
@@ -14,26 +15,42 @@ class Task:
 
 class TaskList:
     def __init__(self):
-        self.current_tasks = []
-        self.completed_tasks = []
+        self.tasks_list = []
 
-    # add
+    def get_completed_tasks(self):
+        return [task for task in self.tasks_list if task.is_complete]
+
+    def get_uncompleted_tasks(self):
+        return [task for task in self.tasks_list if not task.is_complete]
+
+    # add task
     def add_task(self, task):
-        self.current_tasks.append(task)
+        self.tasks_list.append(task)
 
-    # remove
+    # remove task
     def finish_task(self, title):
-        for task in self.current_tasks:
+        for task in self.tasks_list:
             if task.title == title:
                 task.toggle_complete()
-                self.current_tasks.remove(task)
-                self.completed_tasks.append(task)
-                return f"{title} has been moved to DONE!"
+                return f"{title} is DONE!"
         return f"{title} is not found..."
+
+    def save_file(self, filename="todo_list.json"):
+        data = [{"Task Title:": task.title, "Description:": task.description, "Completed:": task.is_complete} for task in self.tasks_list]
+        with open(filename, 'w') as fp:
+            fp.write(json.dumps(data))
+
+    def load_file(self, filename="todo_list.json"):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                self.tasks_list = [Task(task["Task Title:"], task["Description:"], task["Completed:"]) for task in data]
+        except FileNotFoundError:
+            print("File not found...")
 
 
 def main():
-    current = TaskList()
+    tasks = TaskList()
 
     while True:
         print("\n+++==++~  TODO  ~++==+++")
@@ -41,38 +58,55 @@ def main():
         print("+ 2. Complete a Task")
         print("+ 3. View Current Tasks")
         print("+ 4. View Completed Tasks")
-        print("+ 5. Exit")
+        print("+ 5. Save Tasks to file")
+        print("+ 6. Load Tasks from file")
+        print("+ 7. Exit")
 
-        option = input("Enter your choice (1-5): ")
+        option = input("\nEnter your choice (1-7): ")
 
         if option == "1":
             title = input("Enter the Title: ")
             description = input("Enter the Description: ")
             task = Task(title, description, is_complete=False)
-            current.add_task(task)
+            tasks.add_task(task)
             print(f"TODO: {title} added successfully!")
 
         elif option == "2":
             title = input("Enter the Title: ")
-            print(current.finish_task(title))
+            print(tasks.finish_task(title))
 
         elif option == "3":
-            for task in current.current_tasks:
+            uncompleted = tasks.get_uncompleted_tasks()
+            if uncompleted:
+                print("Current Tasks:")
+            for task in uncompleted:
                 print(f"Title: {task.title}, Description: {task.description}, Completed: {task.is_complete}")
+            else:
+                print("Currently there are no Active Tasks")
 
         elif option == "4":
-            if current.completed_tasks:
-                for task in current.completed_tasks:
+            completed = tasks.get_completed_tasks()
+            if completed:
+                print("Completed Tasks:")
+                for task in completed:
                     print(f"Title: {task.title}, Description: {task.description}, Completed: {task.is_complete}")
             else:
                 print("Currently there are no Completed Tasks")
 
         elif option == "5":
+            tasks.save_file()
+            print("Tasks saved to .json")
+
+        elif option == "6":
+            tasks.load_file()
+            print("Tasks loaded from .json")
+
+        elif option == "7":
             print("Goodbye...")
             break
 
         else:
-            print("Invalid option. Please enter a valid option (1-5).")
+            print("Invalid option. Please enter a valid option (1-7).")
 
 
 if __name__ in "__main__":

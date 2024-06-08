1c1f23fd382ec803b07783befb74b71b52bb63fe
mite404 <ethana2008@gmail.com>
2024-06-08 19:21:52 -0400

    Added CLI styling & better encapsulation.

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 #    CLI TO DO App
 #
 import json
+from rich.console import Console
+from rich.traceback import install
+
+install()
+console = Console()
+
 
 class Task:
     def __init__(self, title: str, description: str, is_complete: bool):
@@ -24,16 +30,20 @@ class TaskList:
         return [task for task in self.tasks_list if not task.is_complete]
 
     # add task
-    def add_task(self, task):
-        self.tasks_list.append(task)
+    def add_task(self, task, title):
+        if any(task.title == t.title for t in self.tasks_list):
+            console.print(f"ERROR: {title} already exists, ya dummy!", style="bold red")
+        else:
+            self.tasks_list.append(task)
+            console.print(f"TODO: {title} added successfully!")
 
     # remove task
     def finish_task(self, title):
         for task in self.tasks_list:
             if task.title == title:
                 task.toggle_complete()
-                return f"{title} is DONE!"
-        return f"{title} is not found..."
+                return console.print(f"{title} is DONE!", style="bold green")
+        return console.print(f"{title} is not found...", style="bold red")
 
     def save_file(self, filename="todo_list.json"):
         data = [{"Task Title:": task.title, "Description:": task.description, "Completed:": task.is_complete} for task in self.tasks_list]
@@ -46,21 +56,22 @@ class TaskList:
                 data = json.load(file)
                 self.tasks_list = [Task(task["Task Title:"], task["Description:"], task["Completed:"]) for task in data]
         except FileNotFoundError:
-            print("File not found...")
+            console.print("File not found...", style="bold red")
 
 
 def main():
     tasks = TaskList()
+    tasks.load_file()
 
     while True:
-        print("\n+++==++~  TODO  ~++==+++")
-        print("+ 1. Add a New Task")
-        print("+ 2. Complete a Task")
-        print("+ 3. View Current Tasks")
-        print("+ 4. View Completed Tasks")
-        print("+ 5. Save Tasks to file")
-        print("+ 6. Load Tasks from file")
-        print("+ 7. Exit")
+        console.print("\n+++==++~  TODO  ~++==+++", style="bold italic cyan")
+        console.print("+ 1. Add a New Task", style="italic blue")
+        console.print("+ 2. Complete a Task", style="italic white")
+        console.print("+ 3. View Current Tasks", style="italic green")
+        console.print("+ 4. View Completed Tasks", style="italic green")
+        console.print("+ 5. Save Tasks to file", style="italic yellow")
+        console.print("+ 6. Load Tasks from file", style="italic yellow")
+        console.print("+ 7. Exit", style="italic red")
 
         option = input("\nEnter your choice (1-7): ")
 
@@ -68,45 +79,44 @@ def main():
             title = input("Enter the Title: ")
             description = input("Enter the Description: ")
             task = Task(title, description, is_complete=False)
-            tasks.add_task(task)
-            print(f"TODO: {title} added successfully!")
+            tasks.add_task(task, title)
 
         elif option == "2":
             title = input("Enter the Title: ")
-            print(tasks.finish_task(title))
+            console.print(tasks.finish_task(title))
 
         elif option == "3":
             uncompleted = tasks.get_uncompleted_tasks()
             if uncompleted:
-                print("Current Tasks:")
+                console.print("Current Tasks:")
             for task in uncompleted:
-                print(f"Title: {task.title}, Description: {task.description}, Completed: {task.is_complete}")
+                console.print(f"Title: {task.title}, Description: {task.description}, Completed: {task.is_complete}")
             else:
-                print("Currently there are no Active Tasks")
+                console.print("Currently there are no Active Tasks")
 
         elif option == "4":
             completed = tasks.get_completed_tasks()
             if completed:
-                print("Completed Tasks:")
+                console.print("Completed Tasks:")
                 for task in completed:
-                    print(f"Title: {task.title}, Description: {task.description}, Completed: {task.is_complete}")
+                    console.print(f"Title: {task.title}, Description: {task.description}, Completed: {task.is_complete}")
             else:
-                print("Currently there are no Completed Tasks")
+                console.print("Currently there are no Completed Tasks")
 
         elif option == "5":
             tasks.save_file()
-            print("Tasks saved to .json")
+            console.print("Tasks saved to .json")
 
         elif option == "6":
             tasks.load_file()
-            print("Tasks loaded from .json")
+            console.print("Tasks loaded from .json")
 
         elif option == "7":
-            print("Goodbye...")
+            console.print("Goodbye...")
             break
 
         else:
-            print("Invalid option. Please enter a valid option (1-7).")
+            console.print("Invalid option. Please enter a valid option (1-7).")
 
 
 if __name__ in "__main__":
